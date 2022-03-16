2e7760cab186f68f3d8a7af3d568243868450051
wonhs001 <wonhs001@gmail.com>
2022-03-16 14:02:39 +0900

    commit

---
+++ TodoApp/TodoApp.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Models;
+
+namespace TodoApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITodoRepository _repository = new TodoRepositoryInMemory();
+            List<Todo> todos = new List<Todo>();
+            todos = _repository.GetAll();
+
+            foreach (var rv in todos)
+            {
+                Console.WriteLine($"[{rv.Id}] {rv.Title} : {rv.IsDone}");
+            }
+
+            Console.WriteLine($"---------------------------");
+
+            Todo todo = new Todo() { Title = "Database", IsDone = true };
+            _repository.Add(todo);
+            todos = _repository.GetAll();
+
+            foreach (var rv in todos)
+            {
+                Console.WriteLine($"[{rv.Id}] {rv.Title} : {rv.IsDone}");
+            }
+        }
+    }
+}
